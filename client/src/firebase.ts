c2a0abe732321eccad9aa9440b404077820a8634
Kanduriavinash <kanduriavinashdj9666@gmail.com>
2026-08-29 00:53:36 +0530

    Update Firebase web app configuration

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,15 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBfGA1wF2-Ip9XdJCKF00_a-34lNNndGpg",
+  apiKey: "AIzaSyBfGA1wF2-Ip9XdJCKF00_a-34lNNndGPg",
   authDomain: "peershare-3fbcb.firebaseapp.com",
   databaseURL: "https://peershare-3fbcb-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "peershare-3fbcb",
   storageBucket: "peershare-3fbcb.firebasestorage.app",
   messagingSenderId: "430998628757",
-  appId: "1:430998628757:web:af609889a72a42daee9c",
-  measurementId: "G-9YMWZ8W35L",
+  appId: "1:430998628757:web:46d513a960d9d72cdaee9c",
+  measurementId: "G-PBNPKB0Y4W"
 };
 
 const app = initializeApp(firebaseConfig);
